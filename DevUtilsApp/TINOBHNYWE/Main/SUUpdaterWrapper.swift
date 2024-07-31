69bf50838966c89b01457df59961f256c922f83d
Gary Lin <garynil1635@gmail.com>
2024-07-31 23:10:51 +0800

    Remove SUUpdater

--- DevUtilsApp/TINOBHNYWE/Main/SUUpdaterWrapper.swift
+++ DevUtilsApp/TINOBHNYWE/Main/SUUpdaterWrapper.swift
@@ -7,23 +7,3 @@
 //
 
 import Foundation
-#if NO_SPARKLE
-#else
-import Sparkle
-#endif
-
-#if NO_SPARKLE
-// A dummy class that is compatible with SUUpdater.
-class SUUpdaterWrapper: NSObject {
-  @IBOutlet
-  public var automaticallyChecksForUpdates: NSObject?
-  @IBOutlet
-  public var updateCheckInterval: NSObject?
-  @IBAction
-  func checkForUpdates(_ sender: Any) {
-  }
-}
-#else
-class SUUpdaterWrapper: SUUpdater {
-}
-#endif
